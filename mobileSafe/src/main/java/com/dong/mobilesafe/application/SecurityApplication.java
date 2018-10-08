987f57c6d6f339538d4d4de5452fa1bf5804734f
liaojidong <liaojidong@kingsoft.com>
2018-10-08 16:22:16 +0800

    修复编译错误

--- mobileSafe/src/main/java/com/dong/mobilesafe/application/SecurityApplication.java
+++ mobileSafe/src/main/java/com/dong/mobilesafe/application/SecurityApplication.java
@@ -1,21 +1,16 @@
 package com.dong.mobilesafe.application;
 
 import android.app.Application;
-import android.content.Intent;
 
-import com.dong.mobilesafe.constant.SpKey;
-import com.dong.mobilesafe.service.AppLockService;
-import com.dong.mobilesafe.service.FloatWindowService;
-import com.dong.mobilesafe.service.TrafficMonitorService;
+import com.dong.mobilesafe.BuildConfig;
 import com.dong.mobilesafe.utils.APPUtils;
-import com.dong.mobilesafe.utils.SharedPreferencesManager;
 import com.dong.mobilesafe.utils.security.SecurityCheckManager;
 
 /**
  * Created by liaojd on 2016/6/27.
  */
 public class SecurityApplication extends Application{
-    public boolean isReleaseVersion = true;
+    public boolean isReleaseVersion = !BuildConfig.DEBUG;
     private static SecurityApplication instance;
 
     @Override
